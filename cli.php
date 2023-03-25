31694bde40f119e08e314e9fd76c0dbff6b1beea
GummerD <denis_gummer@mail.ru>
2023-03-25 12:23:37 +0300

    Небольшие изменения в cli.php

--- cli.php
+++ cli.php
@@ -8,29 +8,35 @@ use GummerD\PHPnew\Models\Comment;
 require_once __DIR__ . "/vendor/autoload.php";
 
 $facker = Factory::create('ru_Ru');
+$user =  new User(
+    $facker->randomNumber(),
+    $facker->firstName(),
+    $facker->lastName()
+);
+
+$post = new Post(
+    $facker->randomNumber(),
+    $facker->randomNumber(),
+    $facker->title(),
+    $facker->text()
+);
+
+$comment = new Comment(
+    $facker->randomNumber(),
+    $facker->randomNumber(),
+    $facker->randomNumber(),
+    $facker->text()
+);
+
 
 switch ($argv) {
     case $argv[1] === 'user':
-        echo new User(
-            $facker->randomNumber(),
-            $facker->firstName(),
-            $facker->lastName()
-        );
+        echo $user;
         break;
     case $argv[1] === 'post':
-        echo new Post(
-            $facker->randomNumber(),
-            $facker->randomNumber(),
-            $facker->title(),
-            $facker->text()
-        );
+        echo $post;
         break;
     case $argv[1] === 'comment':
-        echo new Comment(
-            $facker->randomNumber(),
-            $facker->randomNumber(),
-            $facker->randomNumber(),
-            $facker->text()
-        );
+        echo $comment;
         break;
 }
